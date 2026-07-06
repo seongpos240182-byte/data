d5a8670128f4c18877eb55cb0245618de0eb5e6a
seongpos240182-byte <seongpo.s240182@ggh.goe.go.kr>
2026-07-06 19:28:40 +0900

    Update 4_다중선택.py

--- pages/4_다중선택.py
+++ pages/4_다중선택.py
@@ -8,3 +8,20 @@ options = st.multiselect(
      ['노랑', '빨강'])
 
 st.write('당신이 선택한 색상:', options)
+#위에 이어서 붙여넣기 하기
+st.header('체크박스')
+
+st.write ('주문하고 싶은 것이 무엇인가요?')
+
+icecream = st.checkbox('아이스크림')
+coffee = st.checkbox('커피')
+cola = st.checkbox('콜라')
+
+if icecream:
+     st.write("좋아요! 여기 더 많은 🍦")
+
+if coffee: 
+     st.write("알겠습니다, 여기 커피 있어요 ☕")
+
+if cola:
+     st.write("여기 있어요 🥤")

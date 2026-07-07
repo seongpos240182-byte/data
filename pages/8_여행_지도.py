8ed7b58ee2409226f0fbb908ec221737875535bb
seongpos240182-byte <seongpo.s240182@ggh.goe.go.kr>
2026-07-07 10:19:01 +0900

    Update 8_여행_지도.py

--- pages/8_여행_지도.py
+++ pages/8_여행_지도.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # 페이지 제목 설정
 st.title(" 오늘밤은 girls' night!!")
-st.subheader("오늘 야식 메뉴 고르기")
+st.subheader("야식은 선택이 아닌 필수!")
 
 
 # index=None과 placeholder를 제거하고 기본값(0번째 항목)으로 설정
@@ -13,6 +13,9 @@ choice = st.radio(
 
 
 name = st.text_input('사이드로 뭐 먹을래? : ')
+
 st.subheader("오늘 밤 우리의 도파민은?")
 menu = st.selectbox('메인 놀이를 선택해주세요 👇:', ['보드게임','수다 떨기','아이돌 직캠 시청','공포영화 보기','산책하기'])
 name = st.text_input('또 하고 싶은 거 없어? : ')
+
+st.subheader("그날 우리의 스타일을 정해보자!")
